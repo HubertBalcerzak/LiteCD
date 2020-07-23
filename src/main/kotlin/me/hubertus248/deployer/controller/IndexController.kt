531f34b7bdefaf754405627a694650be45a61e1e
Hubert Balcerzak <hubertbalc@gmail.com>
2020-07-23 16:41:11 +0200

    fix index redirect

--- src/main/kotlin/me/hubertus248/deployer/controller/IndexController.kt
+++ src/main/kotlin/me/hubertus248/deployer/controller/IndexController.kt
@@ -2,20 +2,24 @@ package me.hubertus248.deployer.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.security.Principal
 import javax.servlet.http.HttpServletRequest
 
 @Controller
 class IndexController {
 
     @GetMapping("/")
-    fun index(): String = "index"
+    fun index(principal: Principal?): String {
+        if (principal != null) return "redirect:/apps"
+        return "index"
+    }
 
 //    @GetMapping("/apps")
 //    fun appList(): String = "apps"
 
     //TODO use POST /sso/logout instead
     @GetMapping("/logout")
-    fun logout(request: HttpServletRequest): String{
+    fun logout(request: HttpServletRequest): String {
         request.logout()
         return "index"
     }
