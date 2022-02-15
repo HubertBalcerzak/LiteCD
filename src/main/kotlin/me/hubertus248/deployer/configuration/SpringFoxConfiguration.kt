4da234ef684fb400c78d0593380c2de9bc5f5f60
Hubert Balcerzak <hubertbalc@gmail.com>
2022-02-16 00:31:11 +0100

    Add swagger (#10)

---
+++ src/main/kotlin/me/hubertus248/deployer/configuration/SpringFoxConfiguration.kt
@@ -0,0 +1,19 @@
+package me.hubertus248.deployer.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+class SpringFoxConfiguration {
+
+    @Bean
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.ant("/api/**"))
+        .build()
+}
