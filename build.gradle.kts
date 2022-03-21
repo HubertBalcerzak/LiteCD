d332c21b6b9b15b6cf2b6dd712ab2e88757dadf2
Hubert Balcerzak <hubertbalc@gmail.com>
2022-03-21 01:32:02 +0100

    reformat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.1"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
-	kotlin("plugin.jpa") version "1.6.10"
-	kotlin("kapt") version "1.6.10"
+    id("org.springframework.boot") version "2.5.1"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
+    kotlin("kapt") version "1.6.10"
 }
 
 group = "me.hubertus248"
@@ -18,19 +18,19 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("commons-fileupload:commons-fileupload:1.3.3")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.mapstruct:mapstruct:1.4.2.Final")
-	implementation("io.springfox:springfox-boot-starter:3.0.0")
-	kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
-	kapt("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("commons-fileupload:commons-fileupload:1.3.3")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.mapstruct:mapstruct:1.4.2.Final")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
